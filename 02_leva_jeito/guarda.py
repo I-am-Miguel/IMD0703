93ec8681e0f92f35c2238f103ff16bf57cd92255
Miguel <miguelwelligton@gmail.com>
2018-09-09 22:52:45 -0300

    criação e remoção de diretório dinâmico

--- 02_leva_jeito/guarda.py
+++ 02_leva_jeito/guarda.py
@@ -10,6 +10,21 @@ diff_base = "diff.json"
 report_file = dir_base + report_base
 
 
+def create_dir(dir_base=dir_base):
+    if not os.path.isdir(os.path.abspath(dir_base)):
+        os.mkdir(os.path.abspath(dir_base))
+
+
+def delete_dir(dir_base=dir_base):
+    if os.path.isdir(os.path.abspath(dir_base)):
+
+        for raiz, diretorios, arquivos in os.walk(os.path.abspath(dir_base)):
+            for arquivo in arquivos:
+                os.remove(os.path.join(raiz, arquivo))
+
+        os.removedirs(os.path.abspath(dir_base))
+
+
 def filehash(filepath, blocksize=4096):
     sha = hashlib.sha256()
     with open(filepath, 'rb') as fp:
@@ -49,42 +64,60 @@ def scanner_dir(path_name):
     return dict_file
 
 
-
 def dict_report(path_name):
     save_report(scanner_dir(path_name))
 
 
-
-def dict_diff(path_name):
+def dict_diff(path_name, output_file):
     dict_file = scanner_dir(path_name)
     report = load_report()
 
-    return calculate_dict_diff(report,dict_file)
+    return calculate_dict_diff(report, dict_file, output_file)
 
 
-
-def calculate_dict_diff(dict_base, dict_file):
+def calculate_dict_diff(dict_base, dict_file, output_file=None):
     data = {}
-    data['deleted'] = list(set(dict_file) - set(dict_base))
-    data['created'] = list(set(dict_base) - set(dict_file))
+    data['created'] = list(set(dict_file) - set(dict_base))
+    data['deleted'] = list(set(dict_base) - set(dict_file))
     data['updated'] = []
 
     for f in set(dict_file).intersection(set(dict_base)):
         if dict_base[f] != dict_file[f]:
             data['updated'].append(f)
 
-    with open(os.path.abspath(dir_base + diff_base), 'w') as diff:
+    archive = dir_base + diff_base
+    if output_file is not None:
+        archive = dir_base + output_file
+
+    with open(os.path.abspath(archive), 'w') as diff:
         diff.write(str(data))
         diff.close()
 
+    if output_file is None:
+        print(data)
+
 
 if __name__ == '__main__':
+    create_dir()
+
     metodo = sys.argv[1]
     opcao = sys.argv[2]
+    pasta = None
+    saida = None
+    try:
+        pasta = sys.argv[3]
+        saida = sys.argv[4]
+    except:
+        pass
+
     if metodo == "-hash" and opcao == '-i':
-        dict_report(sys.argv[3])
+        dict_report(pasta)
+
     elif metodo == "-hash" and opcao == '-t':
-        dict_diff(sys.argv[3])
+        dict_diff(pasta, saida)
+
+    elif opcao == '-x':
+        delete_dir()
 
     else:
         print("Comando inválido!")
